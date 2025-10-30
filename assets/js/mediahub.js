599724f32b3df63e5fb152f9c773b161ce58607b
MohamedMesto <mohamedmesto111@gmail.com>
2025-10-30 23:44:29 +0100

    adding media hub page, functions

---
+++ assets/js/mediahub.js
@@ -0,0 +1,4 @@
+// For now, no dynamic behavior is needed.
+// This file is included for future features like adding videos dynamically,
+// YouTube API integration, or lazy-loading enhancements.
+console.log("Media Hub loaded successfully!");
